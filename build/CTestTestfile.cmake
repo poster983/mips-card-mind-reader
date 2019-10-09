0b38bd206e151d87cbd93e9c6ec99d35441eaabb
Philip Martin <philipscottmartin01@gmail.com>
2019-10-09 16:57:56 -0500

    Removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/philip/Documents/Google Drive/Development/repos/CS_3340_Proj
-# Build directory: /home/philip/Documents/Google Drive/Development/repos/CS_3340_Proj/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
